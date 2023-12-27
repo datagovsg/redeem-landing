103fd872aa053e78c8570ae8534ad60cbab6cc98
seanjyjy <seanljy98@gmail.com>
2023-12-27 11:56:39 +0800

    feat: remove troll text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 'use client'
 
-import { Text, VStack } from '@chakra-ui/react'
+import { VStack } from '@chakra-ui/react'
 
 export default function Home() {
-  return (
-    <VStack as="main" align="stretch" flexGrow={1} spacing={0}>
-      <Text>Home page bro!</Text>
-    </VStack>
-  )
+  return <VStack as="main" align="stretch" flexGrow={1} spacing={0} />
 }
