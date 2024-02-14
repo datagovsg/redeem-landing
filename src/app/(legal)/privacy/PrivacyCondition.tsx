f5c62d3436816ae8ff768722ca3f8ec71b16a1b4
seanjyjy <seanljy98@gmail.com>
2024-02-14 13:45:10 +0800

    feat: update term of use

--- src/app/(legal)/privacy/PrivacyCondition.tsx
+++ src/app/(legal)/privacy/PrivacyCondition.tsx
@@ -1,7 +1,7 @@
 import { Center, Text, VStack } from '@chakra-ui/react'
 
-import { markdownLikeRender } from '../../helper'
-import LegalText from '../../LegalText'
+import { markdownLikeRender } from '../helper'
+import LegalText from '../LegalText'
 
 import { ANNEX, PRIVACY } from './constants'
 
--- src/app/(legal)/terms-of-use/TermOfUseHeader.tsx
+++ src/app/(legal)/terms-of-use/TermOfUseHeader.tsx
@@ -4,7 +4,7 @@ const TermOfUseHeader = () => {
   return (
     <CommonLegalHeader
       header="Terms of Use"
-      versionAndUpdatedAt="Version 4.1.1, Updated 29 August 2023"
+      versionAndUpdatedAt="Version 4.1.2, Updated 14 February 2024"
     />
   )
 }
