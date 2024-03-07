325507d3a3e470750584a80e281a7012b9b673fd
seanjyjy <seanljy98@gmail.com>
2024-03-07 13:50:36 +0800

    feat: fix config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           h=h[d]=h[d]||{q:[],onReady:function(c){h.q.push(c)}}
           d=o.createElement(u);d.async=1;d.src=n
           n=o.getElementsByTagName(u)[0];n.parentNode.insertBefore(d,n)
-          })(window,document,'script','https://www.datadoghq-browser-agent.com/us1/v5/datadog-rum.js','DD_RUM')
+          })(window,document,'script','https://www.datadoghq-browser-agent.com/ap1/v5/datadog-rum.js','DD_RUM')
           window.DD_RUM.onReady(function() {
             window.DD_RUM.init({
             clientToken: 'pub7d60f6c91f3e16648383f783bb6506c9',
@@ -35,7 +35,8 @@ export default function RootLayout({
             env: '${process.env.NEXT_PUBLIC_APP_ENV}',
             // Specify a version number to identify the deployed version of your application in Datadog
             version: '1.0.0',
-            sampleRate: 1,
+            sessionSampleRate: 1,
+            sessionReplaySampleRate: 100,
             trackUserInteractions: true,
             defaultPrivacyLevel: 'mask-user-input'
           });
